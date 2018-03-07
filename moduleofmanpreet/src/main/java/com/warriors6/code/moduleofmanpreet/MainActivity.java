b95aa38384756caf2c7ccf8a0007cb0f037096ee
Manpreet Singh <mpschahal16@gmail.com>
2018-03-07 12:36:43 +0530

    Checking push operation by:- Manpreet Singh

--- moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
+++ moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast,makeText(this,"Hello",Toast.LENGTH_SHORT).show();
     }
 }
