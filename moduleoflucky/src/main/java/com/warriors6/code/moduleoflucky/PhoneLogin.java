52a9044df3420edf5acb269ac33b9476d3426423
manpreet <mpschahal16@gmail.com>
2018-03-08 01:22:29 +0530

    Application Icon & Dependency chnged to 27-Manpreet Singh

---
+++ moduleoflucky/src/main/java/com/warriors6/code/moduleoflucky/PhoneLogin.java
@@ -0,0 +1,13 @@
+package com.warriors6.code.moduleoflucky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_login);
+    }
+}
---
+++ moduleoflucky/src/main/java/com/warriors6/code/moduleoflucky/Splash.java
@@ -0,0 +1,30 @@
+package com.warriors6.code.moduleoflucky;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Thread() {
+            public void run() {
+                int waited = 0;
+                while (waited < 500) {
+                    try {
+                        Thread.sleep(100);
+                        waited += 100;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                finish();
+                startActivity(new Intent(Splash.this, PhoneLogin.class));
+            }
+        }.start();
+    }
+}
