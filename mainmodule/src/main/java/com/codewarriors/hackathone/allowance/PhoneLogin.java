e77c783d8719e9df4eba362c1e64a1b272db5baa
manpreet <mpschahal16@gmail.com>
2018-03-08 02:08:00 +0530

    Splashed Merged to MainModule-Manpreet Singh

--- mainmodule/src/main/java/com/codewarriors/hackathone/allowance/PhoneLogin.java
+++ mainmodule/src/main/java/com/codewarriors/hackathone/allowance/PhoneLogin.java
@@ -3,11 +3,11 @@ package com.codewarriors.hackathone.allowance;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Firstlogin extends AppCompatActivity {
+public class PhoneLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firstlogin);
+        setContentView(R.layout.activity_phonelogin);
     }
 }
---
+++ mainmodule/src/main/java/com/codewarriors/hackathone/allowance/Splash.java
@@ -0,0 +1,30 @@
+package com.codewarriors.hackathone.allowance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Thread() {
+            public void run() {
+                int waited = 0;
+                while (waited < 1500) {
+                    try {
+                        Thread.sleep(100);
+                        waited += 100;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                finish();
+                startActivity(new Intent(Splash.this, PhoneLogin.class));
+            }
+        }.start();
+    }
+}
