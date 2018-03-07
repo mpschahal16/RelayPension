2d0e8f31b44c7472b320c186fc0e4abbbeb44353
manpreet <mpschahal16@gmail.com>
2018-03-07 13:00:58 +0530

    Everything Back to Initial state:-Manpreet Singh

--- moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
+++ moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Hello world",Toast.LENGTH_SHORT).show();
     }
 }
