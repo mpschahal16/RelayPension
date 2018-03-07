03eb212d54353172a054df2985230f1875834ef3
Manpreet Singh <mpsvhahal16@gmail.com>
2018-03-08 00:13:03 +0530

    splash screen by lucky

--- moduleoflucky/src/main/java/com/warriors6/code/moduleoflucky/MainActivity.java
+++ moduleoflucky/src/main/java/com/warriors6/code/moduleoflucky/MainActivity.java
@@ -11,14 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Thread splash = new Thread()
+        Thread splash;
+        splash = new Thread()
         {public void run()
         {try {
             Thread.sleep(3500);
         }catch (InterruptedException e) {
             e.printStackTrace();
         }finally {
-            startActivity(new Intent(MainActivity.this,AppCompatActivity.class));
+            startActivity(new Intent(MainActivity.this,MainActivity.class));
             finish();
         }
         }
