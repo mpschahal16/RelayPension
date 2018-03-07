2c4a80ea6945bc2c42949341b7f1afa3a77297ad
Manpreet Singh <mpschahal16@gmail.com>
2018-03-07 12:49:30 +0530

    Update MainActivity.java

--- moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
+++ moduleofmanpreet/src/main/java/com/warriors6/code/moduleofmanpreet/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Hello world",Toast.LENGTH_SHORT).show();
     }
 }
