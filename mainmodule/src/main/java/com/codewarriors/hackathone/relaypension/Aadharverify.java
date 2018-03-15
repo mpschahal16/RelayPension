647c4decbf0d839e99c327cca817504e1de6e97b
manpreet <mpschahal16@gmail.com>
2018-03-16 03:58:54 +0530

    Msg69 Implimentation completed Now we are able to verify aadhar by otp-Manpreet Singh

--- mainmodule/src/main/java/com/codewarriors/hackathone/relaypension/Aadharverify.java
+++ mainmodule/src/main/java/com/codewarriors/hackathone/relaypension/Aadharverify.java
@@ -1,11 +1,16 @@
 package com.codewarriors.hackathone.relaypension;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.hardware.ConsumerIrManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +19,26 @@ import android.widget.Toast;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.codewarriors.hackathone.relaypension.customvariablesforparsing.ConstituencyHelperClass;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.msg91.sendotp.library.SendOtpVerification;
+import com.msg91.sendotp.library.Verification;
+import com.msg91.sendotp.library.VerificationListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Aadharverify extends AppCompatActivity implements View.OnClickListener,DialogInterface.OnClickListener {
+public class Aadharverify extends AppCompatActivity implements View.OnClickListener,DialogInterface.OnClickListener,VerificationListener{
    EditText aadharnoet,consituencyet,anualfamilyincomeet,otpaadharverifyet;
 
    ArrayList<String> listofconsituency;
    ArrayList<Integer> listofmaxlimitinconsituency;
+
+   String adharno,constituency;
+   int familyanylin;
 
 
    TextInputLayout textInputLayout;
@@ -33,7 +48,11 @@ public class Aadharverify extends AppCompatActivity implements View.OnClickListe
    Button sendotptophonebt,verifyotpbt;
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("stubofuid/");
 
+    ProgressDialog progressDialog;
     //Timeraa timeraa;
+
+
+    Verification msg69var;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +85,8 @@ public class Aadharverify extends AppCompatActivity implements View.OnClickListe
         sendotptophonebt.setOnClickListener(this);
         verifyotpbt.setOnClickListener(this);
 
+        progressDialog=new ProgressDialog(this);
+
         awesomeValidation=new AwesomeValidation(ValidationStyle.COLORATION);
         otp=new AwesomeValidation(ValidationStyle.COLORATION);
 
@@ -96,7 +117,7 @@ public class Aadharverify extends AppCompatActivity implements View.OnClickListe
                     if(listofconsituency.contains(constituency)&&familysalery<=100000)
                     {
 
-                        Toast.makeText(getApplicationContext(),"Sucess",Toast.LENGTH_LONG).show();
+                        checkinfoinstub();
 
                     }
                     else
@@ -117,12 +138,13 @@ public class Aadharverify extends AppCompatActivity implements View.OnClickListe
 
                     }
                 }
-               // sendotptoaddharinno();
                 break;
             }
             case R.id.verifyaadotpbt:
             {
-              //  verifyaddinno();
+                if(otp.validate()) {
+                    verifyaddinno();
+                }
                 break;
             }
         }
@@ -134,65 +156,101 @@ public class Aadharverify extends AppCompatActivity implements View.OnClickListe
 
     }
 
-   /* private void verifyaddinno() {
-
-        String st=otpno.getText().toString().trim();
-        if(TextUtils.isEmpty(st))
-        {
-            errorotp.setVisibility(View.VISIBLE);
-            errorotp.setText(R.string.invalid_otp);
-
-
-        }
-        else
-        {
-
-            Intent it=new Intent(Aadharverify.this,FIllform.class);
-            it.putExtra("adharno",adharnotonextactivity);
-            timeraa.cancel(true);
-            startActivity(it);
-        }
-    }*/
-
-  /*  private void sendotptoaddharinno() {
-
-        String st=adharno.getText().toString().trim();
-        if(awesomeValidation.validate())
-        {
-            adharnotonextactivity=st;
-            DatabaseReference mr=mDatabase.child(adharnotonextactivity);
-
-            //PRESENTLY WE ARE EXTRACTING AADHAR NO FROM AADHAR NO AND SENDING IT TO FORM FILL ACTIVITY
-            //BUT WE NEED TO FETCH PHONE NO ATTACHED TO THAT AADHAAR AND VERIFY THAT PHONE NO WITH OTP AND THEN SEND THAT AADHAR NO TO NEXT ACTIVITY
-            mr.child("aadharNo").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists()) {
-                        String userpno = dataSnapshot.getValue().toString();
-                        sendOTPtoPhoneNO(userpno);
-                    }
-                    else
-                    {
-                        Toast.makeText(getApplicationContext(),"Aadhar Does Not exsist in DB",Toast.LENGTH_LONG).show();
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    Toast.makeText(getApplicationContext(),"Check Network/Db error",Toast.LENGTH_LONG).show();
-
-                }
-            });
-        }
+   private void verifyaddinno() {
+       String s=otpaadharverifyet.getText().toString();
+       msg69var.verify(s);
     }
 
+    private void checkinfoinstub() {
+        final String st=aadharnoet.getText().toString().trim();
+        constituency= consituencyet.getText().toString();
+        familyanylin=Integer.parseInt(anualfamilyincomeet.getText().toString());
+        DatabaseReference mr=mDatabase.child(st);
+        progressDialog.setMessage("Checking Info");
+        progressDialog.show();
+        //PRESENTLY WE ARE EXTRACTING AADHAR NO FROM AADHAR NO AND SENDING IT TO FORM FILL ACTIVITY
+        //BUT WE NEED TO FETCH PHONE NO ATTACHED TO THAT AADHAAR AND VERIFY THAT PHONE NO WITH OTP AND THEN SEND THAT AADHAR NO TO NEXT ACTIVITY
+        mr.child("phoneNo").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    String userpno = dataSnapshot.getValue().toString();
+                    Toast.makeText(getApplicationContext(),userpno,Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
+                    adharno=st;
+                    sendOTPtoPhoneNO(userpno);
+                }
+                else
+                {
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(),"Aadhar Does Not exsist in DB",Toast.LENGTH_LONG).show();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),"Check Network/Db error",Toast.LENGTH_LONG).show();
+                }
+        });
+    }
+
+
+
+
+
+
+
+    //msg 69 code for otp verification of aadhar
     private void sendOTPtoPhoneNO(String userpno) {
-        Intent intent=new Intent(Aadharverify.this,FIllform.class);
-        intent.putExtra("phoneno",userpno);
-        startActivity(intent);
+
+        msg69var = SendOtpVerification.createSmsVerification
+                (SendOtpVerification
+                        .config("+91" + userpno)
+                        .context(this)
+                        .autoVerification(true)
+                        .build(), this);
+
+        msg69var.initiate();
+      //  onInitiated("CCC");
+
     }
 
-    @SuppressLint("StaticFieldLeak")
+    @Override
+    public void onInitiated(String response) {
+        otpaadharverifyet.setVisibility(View.VISIBLE);
+        verifyotpbt.setVisibility(View.VISIBLE);
+        textInputLayout.setVisibility(View.VISIBLE);
+
+        Log.d("msg69","onInitiated"+response);
+    }
+
+    @Override
+    public void onInitiationFailed(Exception paramException) {
+        Toast.makeText(getApplication(),"Check ur Network",Toast.LENGTH_LONG).show();
+        Log.d("msg69","onInitiatedfailed");
+
+    }
+
+    @Override
+    public void onVerified(String response) {
+        Log.d("msg69","onverified");
+
+        Toast.makeText(getApplication(),"Sucess",Toast.LENGTH_LONG).show();
+
+
+        //here we will write the code to update database that user has verified itself
+
+
+      /*  Intent intent=new Intent(Aadharverify.this,FIllform.class);
+        startActivity(intent);*/
+
+    }
+
+    @Override
+    public void onVerificationFailed(Exception paramException) {
+        Log.d("msg69","onverifiedfailed");
+    }
+
+   /* @SuppressLint("StaticFieldLeak")
     private class Timeraa extends AsyncTask<String, Integer, String> {
 
         @Override
